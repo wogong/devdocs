adeb2462bc4dfdbc62abe21c7f4b8e4efe21db40
Peiran Yao <xavieryao@me.com>
2020-05-31 21:42:40 -0600

    remove katex-html

--- lib/docs/filters/pytorch/clean_html.rb
+++ lib/docs/filters/pytorch/clean_html.rb
@@ -3,6 +3,8 @@ module Docs
     class CleanHtmlFilter < Filter
       def call
         @doc = at_css('.pytorch-article')
+        # Show katex-mathml nodes and remove katex-html nodes
+        css('.katex-html').remove
         doc
       end
     end
